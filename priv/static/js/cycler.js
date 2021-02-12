a65b8f0b12f5a612b6effe48177a0dea5725c266
Jesse Gumm <gumm@sigma-star.com>
2021-02-12 16:18:09 -0600

    Fix JS bug when elements removed from DOM

--- priv/static/js/cycler.js
+++ priv/static/js/cycler.js
@@ -1,7 +1,7 @@
 function init_cycler_flip(id, images, linger) {
 	var iterator = 0;
 
-	setInterval(function() {
+	var timer = setInterval(function() {
 		if(++iterator >= images.length) {
 			iterator=0;
 		}
@@ -10,16 +10,21 @@ function init_cycler_flip(id, images, linger) {
 		var next_selector = iterator%2==0 ? ".cycler_face img" : ".cycler_back img";
 		var next_selector = ".wfid_" + id + " " + next_selector;
 
-		document.querySelector(next_selector).src=next_image;
-		
-		obj(id).classList.toggle("cycler_flipped");
+		var item = document.querySelector(next_selector);
+		if(item)
+		{
+			item.src=next_image;
+			obj(id).classList.toggle("cycler_flipped");
+		}else{
+			clearInterval(timer);
+		}
 	}, linger);
 }
 
 function init_cycler_blink(id, images, linger, speed) {
 	var iterator = 0;
 
-	setInterval(function() {
+	var timer = setInterval(function() {
 		if(++iterator >= images.length) {
 			iterator=0;
 		}
@@ -27,7 +32,12 @@ function init_cycler_blink(id, images, linger, speed) {
 		var next_image = images[iterator];
 	
 		var next_selector = ".wfid_" + id + " .cycler_face img";
-		document.querySelector(next_selector).src=next_image;
+		var item = document.querySelector(next_selector);
+		if(item) {
+			item.src=next_image;
+		}else{
+			clearInterval(timer);
+		}
 //		$(next_selector)
 //			.hide({effect: "slide", speed: speed/2})
 //			.next()
